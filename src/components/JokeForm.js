3a046e057080b43bd4f2cbee2d5a1802dba86cae
bradsimpson213 <bsimpson@appacademy.io>
2021-02-15 22:19:41 -0500

    working on new joke form

--- src/components/JokeForm.js
+++ src/components/JokeForm.js
@@ -1,5 +1,7 @@
 // React imports
 import React from 'react';
+// Custom hooks
+import useInputState from "../hooks/useInputState"
 // Style imports
 import {createUseStyles} from 'react-jss';
 
@@ -10,17 +12,64 @@ const useStyles = createUseStyles({
         border: '2px solid black',
         borderRadius: '15px',
         backgroundColor: 'azure'
+    },
+    submitform: {
+        display: 'flex',
+        flexDirection: 'column'
     }
 });
 
 const JokeForm = () => {
     const classes = useStyles();
+    const [jokeBody, setJokeBody, resetJokeBody] = useInputState();
+    const [punchline, setPunchline, resetPunchLine] = useInputState();
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log("Yay you submitted")
+        resetJokeBody()
+        resetPunchLine()
+
+    }
 
     return (
         <div className={ classes.root }>
             <h1>Submit your new jobes here...</h1>
-            <form>
-                
+            <form 
+                className={ classes.submitForm }
+                onSubmit={ handleSubmit }
+            >
+                <div>
+                    <label 
+                        for="joktext"
+                    >
+                        Joke body: 
+                    </label>
+                    <textarea 
+                        name="joketext" 
+                        type='text'
+                        value={ jokeBody }
+                        onChange={ setJokeBody }
+                    />
+                </div>
+                <div>
+                    <label 
+                        for="punchline" 
+                    >
+                        Joke body: 
+                    </label>
+                    <textarea 
+                        name="punchline"
+                        type="text"
+                        value={ punchline }
+                        onChange={ setPunchline }
+                    />
+                </div>
+                <button 
+                    type="submit"
+                >
+                    Submit Joke
+                </button>
             </form>
 
         </div>
