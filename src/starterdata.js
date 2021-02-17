502dd9cac2f549cd2bb681e2dcf5b9d16cd0b050
bradsimpson213 <bsimpson@appacademy.io>
2021-02-16 23:12:18 -0500

    working on seed file

---
+++ src/starterdata.js
@@ -0,0 +1,27 @@
+starterJokes = [
+    {
+        jokeId: 1,
+        jokeBody: "What do mermaids wash their fins with?",
+        jokePunchline: "Tide"
+    },
+    {
+        jokeId: 2,
+        jokeBody: "Did you know in King Arthur's time, one of the knights of the round table collected taxes?",
+        jokePunchline: "His name was Sir Charge"
+    },
+    {
+        jokeId: 3,
+        jokeBody: "What did the fried rice say to the shrimp?",
+        jokePunchline: "Don't Wok away from me..."
+    },
+    {
+        jokeId: 4,
+        jokeBody: "",
+        jokePunchline: ""
+    },
+    {
+        jokeId: 4,
+        jokeBody: "",
+        jokePunchline: ""
+    },
+]
