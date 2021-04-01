6b86083857cf2ba8f265e9cb1a1aff375c5164ff
bradsimpson213 <bsimpson@appacademy.io>
2021-04-01 10:39:48 -0400

    db connected

--- dad-jokes-server/app.js
+++ dad-jokes-server/app.js
@@ -7,6 +7,26 @@ const users = require('./routes/users');
 app.use('/users', users);
 
 
+const { sequelize } = require("./models");
+
+async function main() {
+  try {
+    await sequelize.authenticate();
+  } catch (e) {
+    console.log("Database connection failure.");
+    console.log(e);
+    return;
+  }
+
+  console.log("Database connection success!");
+  console.log("Sequelize is ready to use!");
+
+  // Close database connection when done with it.
+  await sequelize.close();
+}
+
+main();
+
 const { port } = require('./config');
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
